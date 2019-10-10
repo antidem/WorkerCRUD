ed89e4c0c70e25972ab64d362fe96101b74e5f3e
Freeman <Freeman@CTHULHU>
2019-10-10 16:27:41 +0400

    Add project files.

---
+++ WorkerCRUD/Models/Worker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WorkerCRUD.Models
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(80, MinimumLength = 1)]
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+        [Range(1, 2)]
+        public int Gender { get; set; }
+        [StringLength(150, MinimumLength = 1)]
+        public string Specialization { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
---
+++ WorkerCRUD/Models/WorkerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorkerCRUD.Models
+{
+    public class WorkerContext : DbContext
+    {
+        public DbSet<Worker> Worker { get; set; }
+
+        public WorkerContext(DbContextOptions<WorkerContext> options)
+            : base(options)
+        {
+        }
+    }
+}
